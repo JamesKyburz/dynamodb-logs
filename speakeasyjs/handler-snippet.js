1c7881f8c7430e96e809d2c268722e20ce5c3009
James Kyburz <james.kyburz@gmail.com>
2021-04-23 10:55:22 +0100

    chore: speakeasy snippet tweaks

--- speakeasyjs/handler-snippet.js
+++ speakeasyjs/handler-snippet.js
@@ -1,9 +1,13 @@
-exports.handler = async function user (event) {
-  const {
-    detail: {
-      key: { sk, pk },
-      payload: { id }
-    }
-  } = event
+exports.handler = async function user ({
+  detail: {
+    type,
+    key: { sk, pk },
+    payload: { id }
+  }
+}) {
+  // partition key (pk)
+  // sort key (sk)
+  // id of event
+  // type of event
   //...
 }
--- speakeasyjs/sqlite-snippet.js
+++ speakeasyjs/sqlite-snippet.js
@@ -1,8 +1,10 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('db')
+
 exports.handler = async function save (event) {
-  if (event.detail['replay-name']) {
-    console.log(`will not save replay ${event.detail['replay-name']} to db`)
+  const { 'replay-name': replayName } = event
+  if (replayName) {
+    console.log(`will not save replay ${replayName} to db`)
     return
   }
   // ...
